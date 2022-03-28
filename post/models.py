bc739388a099e2d4623a05b522e875a632f7f7a9
Victor de Castro <56875087+VictorCastroBR@users.noreply.github.com>
2022-03-28 13:21:57 -0300

    Update models.py

--- post/models.py
+++ post/models.py
@@ -1,7 +1,3 @@
-from pyexpat import model
-from statistics import mode
-from urllib import request
-from venv import create
 from django.db import models
 from django.contrib.auth.models import User
 
